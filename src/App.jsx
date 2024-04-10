b8ed6bfb4bd169580337d22b7ccd3fd06d64f38b
mohamed hassan mohamed <mh3073629@gmail.com>
2024-04-10 02:40:14 +0200

    Finish SetUp of Project and make navbar empty component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+
+      <Navbar/>
+      hero
+      highlight
+    </main>
+   );
+};
+
+export default App;
